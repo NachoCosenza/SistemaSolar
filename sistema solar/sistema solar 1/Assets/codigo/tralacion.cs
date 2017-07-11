a0073bb66f79ae5c33f8cf1b4974ffa630c639da
NachoCosenza <cosenza.lores@hotmail.com>
2017-07-11 17:57:26 -0300

    cosenza
    proyecto sistema solar

---
+++ sistema solar/sistema solar 1/Assets/codigo/tralacion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tralacion : MonoBehaviour {
+
+	public Transform pivot;
+	public float velocidad ;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.RotateAround(pivot.position,Vector3.up,velocidad);
+
+
+
+	}
+}
